fd59e506de2d3ce4b70cacc9c93ebdafbc2b0831
colorwheeloflife <colorwheeloflife@gmail.com>
2017-07-29 17:22:03 -0700

    Added conditional class to clothing sizing field in order to make it only appear when clothing is selected.

--- assets/js/src/components/pages/inventory_gamepad/add_item/fields/sizing/ClothingSizingField.js
+++ assets/js/src/components/pages/inventory_gamepad/add_item/fields/sizing/ClothingSizingField.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ClothingSizeInputGroup from './pieces/ClothingSizeInputGroup';
 
 const ClothingSizingField = (props) => (
-		<div id="clothing_sizing_field" className={'' + (props.selectedCategory.indexOf('Clothing') > -1 ? 'show' : '')}>
+		<div id="clothing_sizing_field" className={'' + (props.selectedCategory.indexOf('Clothing') > -1 ? 'show' : 'hidden')}>
 
 			<div id="add_item_clothing_sizing_bar">
 				<ClothingSizeInputGroup
